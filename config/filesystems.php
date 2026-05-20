9100e97bfd38f325e78c6e7eb9b48d061d4aca00
TechnoGroup10 <ojancok212@gmail.com>
2026-05-20 10:33:09 +0700

    Adjust image storage v2

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,16 +1,49 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Parse LARAVEL_CLOUD_DISK_CONFIG
+|--------------------------------------------------------------------------
+|
+| Laravel Cloud injects disk credentials as a JSON array in the env var
+| LARAVEL_CLOUD_DISK_CONFIG. We decode it here and build an s3-compatible
+| disk entry for each object, then merge into the disks array below.
+|
+*/
+
+$cloudDisks = [];
+
+if ($cloudDiskConfig = env('LARAVEL_CLOUD_DISK_CONFIG')) {
+    $configs = json_decode($cloudDiskConfig, true) ?? [];
+
+    foreach ($configs as $config) {
+        $diskName = $config['disk'] ?? null;
+
+        if (!$diskName) {
+            continue;
+        }
+
+        $cloudDisks[$diskName] = [
+            'driver'                  => 's3',
+            'key'                     => $config['access_key_id'] ?? '',
+            'secret'                  => $config['access_key_secret'] ?? '',
+            'region'                  => $config['default_region'] ?? 'auto',
+            'bucket'                  => $config['bucket'] ?? '',
+            'url'                     => $config['url'] ?? null,
+            'endpoint'                => $config['endpoint'] ?? null,
+            'use_path_style_endpoint' => $config['use_path_style_endpoint'] ?? false,
+            'visibility'              => 'public',
+            'throw'                   => false,
+        ];
+    }
+}
+
 return [
 
     /*
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application. Just store away!
-    |
     */
 
     'default' => env('FILESYSTEM_DISK', 'local'),
@@ -19,54 +52,42 @@ return [
     |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure as many filesystem "disks" as you wish, and you
-    | may even configure multiple disks of the same driver. Defaults have
-    | been set up for each driver as an example of the required values.
-    |
-    | Supported Drivers: "local", "ftp", "sftp", "s3"
-    |
     */
 
-    'disks' => [
+    'disks' => array_merge([
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
-            'throw' => false,
+            'root'   => storage_path('app'),
+            'throw'  => false,
         ],
 
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver'     => 'local',
+            'root'       => storage_path('app/public'),
+            'url'        => env('APP_URL') . '/storage',
             'visibility' => 'public',
-            'throw' => false,
+            'throw'      => false,
         ],
 
         's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'driver'                  => 's3',
+            'key'                     => env('AWS_ACCESS_KEY_ID'),
+            'secret'                  => env('AWS_SECRET_ACCESS_KEY'),
+            'region'                  => env('AWS_DEFAULT_REGION'),
+            'bucket'                  => env('AWS_BUCKET'),
+            'url'                     => env('AWS_URL'),
+            'endpoint'                => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'throw'                   => false,
         ],
 
-    ],
+    ], $cloudDisks),
 
     /*
     |--------------------------------------------------------------------------
     | Symbolic Links
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure the symbolic links that will be created when the
-    | `storage:link` Artisan command is executed. The array keys should be
-    | the locations of the links and the values should be their targets.
-    |
     */
 
     'links' => [
